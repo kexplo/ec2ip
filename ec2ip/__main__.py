8d11ec2cc2cf6819c446ab5b7156240bf521d102
Chanwoong Kim <me@chanwoong.kim>
2019-06-03 05:31:26 +0000

    Add -i, -t, -s options

--- ec2ip/__main__.py
+++ ec2ip/__main__.py
@@ -15,19 +15,28 @@ __all__ = ['cli']
 @click.argument('name')
 @click.option('--find', '-f', is_flag=True)
 @click.option('--verbose', '-v', is_flag=True)
-def cli(name, find, verbose):
+@click.option('--show-instance-id', '-i', is_flag=True, default=False)
+@click.option('--show-tag', '-t', is_flag=True, default=True)
+@click.option('--include-stopped-instances', '-s', is_flag=True)
+def cli(name, find, verbose, show_instance_id, show_tag,
+        include_stopped_instances):
     """Find private ip of EC2 instances.\n
     inspired by yeonghoey's ec2ip.py
     (https://github.com/yeonghoey/dotfiles/blob/master/python/ec2ip.py)
     """
-    found_instances = {}
+    if verbose:
+        show_instance_id = show_tag = True
+    found_instances = list()
     # for region in boto3.client('ec2').describe_regions()['Regions']:
     #     ec2 = boto3.resource('ec2', region_name=region['RegionName'])
-    for region_name in ('ap-northeast-1', 'ap-northeast-2'):
+    filter_instance_states = ['running']
+    if include_stopped_instances:
+        filter_instance_states.append('stopped')
+    for region_name in ('ap-northeast-1', 'ap-northeast-2', 'us-east-1'):
         ec2 = boto3.resource('ec2', region_name=region_name)
         instances = ec2.instances.filter(Filters=[
             {'Name': 'instance-state-name',
-             'Values': ['running']}])
+             'Values': filter_instance_states}])
         for instance in instances:
             if not instance.tags:
                 continue
@@ -39,15 +48,27 @@ def cli(name, find, verbose):
                             if tag['Key'] == 'Name' and tag['Value'] == name]
             if not found_tags:
                 continue
-            found_instances[found_tags[0]] = instance.private_ip_address
-            # found_instances[found_tags[0]] = instance.instance_id
+            name_tag = found_tags[0]
+            found_instances.append((instance.instance_id, name_tag,
+                                    instance.private_ip_address))
 
     if not found_instances:
         print('Not found', file=sys.stderr)
         return 1
-    for tag, private_ip in found_instances.viewitems():
-        if find or verbose:
-            print ('{0}\t{1}'.format(tag, private_ip))
+    column_count = 1
+    if show_instance_id:
+        column_count += 1
+    if show_tag:
+        column_count += 1
+    column_fmt_str = '\t'.join(['{}'] * column_count)
+    for instance_id, tag, private_ip in found_instances:
+        picked_columns = []
+        if show_instance_id:
+            picked_columns.append(instance_id)
+        if show_tag:
+            picked_columns.append(tag)
+        picked_columns.append(private_ip)
+        print (column_fmt_str.format(*picked_columns))
     return 0
 
 
